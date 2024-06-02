ec0f3ea410cc775c29f1c6ae9b16bbc63d0592d3
Trinh Nguyen <137372274+zTrinh121@users.noreply.github.com>
2024-06-02 12:33:46 +0700

    Revert to first commit

--- src/main/java/com/example/service/AdminService.java
+++
@@ -1,8 +0,0 @@
-package com.example.service;
-
-public interface AdminService {
-
-    long getUserCount();
-
-    long getManagement();
-}
--- src/main/java/com/example/service/EmailService.java
+++
@@ -1,6 +0,0 @@
-package com.example.service;
-
-public interface EmailService {
-
-    void sendEmail(String to, String subject, String body);
-}
