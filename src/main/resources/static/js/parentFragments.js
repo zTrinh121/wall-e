daa5a07c993f93c8099b44998d52d35f4a24811c
Trinh Nguyen <137372274+zTrinh121@users.noreply.github.com>
2024-06-15 15:31:50 +0700

    Front end for chat box

--- src/main/resources/static/js/parentFragments.js
+++ src/main/resources/static/js/parentFragments.js
@@ -112,4 +112,95 @@ document.addEventListener("DOMContentLoaded", function () {
             notificationModal.style.display = "none";
         }
     });
+
+    // TOGGLE CHATBOX
+    const chatboxToggle = document.querySelector('.chatbox-toggle');
+    const chatboxMessage = document.querySelector('.chatbox-message-wrapper');
+
+    if (chatboxToggle) {
+        console.log("chatboxToggle element found:", chatboxToggle);
+        chatboxToggle.addEventListener('click', function () {
+            console.log("It show"); // Check if this logs to the console
+            chatboxMessage.classList.toggle('show');
+        });
+    } else {
+        console.error("chatboxToggle element not found");
+    }
+
+    // DROPDOWN TOGGLE
+    const dropdownToggle = document.querySelector('.chatbox-message-dropdown-toggle');
+    const dropdownMenu = document.querySelector('.chatbox-message-dropdown-menu');
+
+    dropdownToggle.addEventListener('click', function () {
+        dropdownMenu.classList.toggle('show');
+    });
+
+    document.addEventListener('click', function (e) {
+        if (!e.target.matches('.chatbox-message-dropdown, .chatbox-message-dropdown *')) {
+            dropdownMenu.classList.remove('show');
+        }
+    });
+
+    // Start chat box
+    // CHATBOX MESSAGE
+    const chatboxMessageWrapper = document.querySelector('.chatbox-message-content');
+    const chatboxNoMessage = document.querySelector('.chatbox-message-no-message');
+
+    chatboxForm.addEventListener('submit', function (e) {
+        e.preventDefault();
+
+        if (isValid(textarea.value)) {
+            writeMessage();
+            setTimeout(autoReply, 1000);
+        }
+    });
+
+    function addZero(num) {
+        return num < 10 ? '0' + num : num;
+    }
+
+    function writeMessage() {
+        const today = new Date();
+        let message = `
+            <div class="chatbox-message-item sent">
+                <span class="chatbox-message-item-text">
+                    ${textarea.value.trim().replace(/\n/g, '<br>\n')}
+                </span>
+                <span class="chatbox-message-item-time">${addZero(today.getHours())}:${addZero(today.getMinutes())}</span>
+            </div>
+        `;
+        chatboxMessageWrapper.insertAdjacentHTML('beforeend', message);
+        chatboxForm.style.alignItems = 'center';
+        textarea.rows = 1;
+        textarea.focus();
+        textarea.value = '';
+        chatboxNoMessage.style.display = 'none';
+        scrollBottom();
+    }
+
+    function autoReply() {
+        const today = new Date();
+        let message = `
+            <div class="chatbox-message-item received">
+                <span class="chatbox-message-item-text">
+                    Thank you for your awesome support!
+                </span>
+                <span class="chatbox-message-item-time">${addZero(today.getHours())}:${addZero(today.getMinutes())}</span>
+            </div>
+        `;
+        chatboxMessageWrapper.insertAdjacentHTML('beforeend', message);
+        scrollBottom();
+    }
+
+    function scrollBottom() {
+        chatboxMessageWrapper.scrollTo(0, chatboxMessageWrapper.scrollHeight);
+    }
+
+    function isValid(value) {
+        let text = value.replace(/\n/g, '');
+        text = text.replace(/\s/g, '');
+
+        return text.length > 0;
+    }
+    // End chat box
 });
