4b5ad1705ce2ccd086e9dbb88c7e5220de2eab13
datphan <tiendat.xpbp@gmail.com>
2024-06-25 02:23:20 +0700

    DB

--- src/main/java/com/example/SWP391_Project/dto/CourseDto.java
+++ src/main/java/com/example/SWP391_Project/dto/CourseDto.java
@@ -16,7 +16,6 @@ public class CourseDto {
     Date endDate;
     int amountOfStudents;
     float courseFee;
-    float totalCourseFee;
     String centerCode;
     String subjectName;
     String teacherCode;
